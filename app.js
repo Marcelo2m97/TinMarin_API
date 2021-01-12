a7f034a0bbde864ee585d656f9f8009561688d4b
Marcelo Martínez <marcelo2m1097@gmail.com>
2021-01-12 16:35:28 -0600

    Setup documentation on swagger

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var apiRouter = require('./routes/api');
+const apiRouter = require('./routes/api');
 
 const { connect } = require('./config/database');
 
 connect();
 
-var app = express();
+const app = express();
 
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Tin Marin API',
+      version: '1.0.0'
+    }
+  },
+  apis: ['app.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
