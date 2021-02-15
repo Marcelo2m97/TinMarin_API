155f1c001e62d943d6945f33d2f8d0a000b2b00f
Marcelo Martínez <marcelo2m1097@gmail.com>
2021-02-14 21:29:15 -0600

    Add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const swaggerDoc = require('./swagger.json');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const AuthMiddleware = require('./middlewares/Auth');
 
@@ -16,11 +17,15 @@ connect();
 
 const app = express();
 
+app.use(cors());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
+
 
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use(logger('dev'));
